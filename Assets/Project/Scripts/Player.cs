730b6aaaad2fcb69e58f7f04839bbeff7022301a
Komodeo <onlythebest@gmail.com>
2020-06-08 14:45:45 -0400

    lesson 2
    configure ball to follow player

---
+++ Assets/Project/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+
+{
+
+    public GameObject ball;
+    public GameObject playerCamera;
+
+    public float ballDistance = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ball.transform.position = playerCamera.transform.position + (playerCamera.transform.forward * ballDistance);
+    }
+}
