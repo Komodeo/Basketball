529b447334f4dc2a116c9faa9c5d167e1a1278bf
Komodeo <onlythebest@gmail.com>
2020-06-09 14:27:13 -0400

    lesson 4
    make ball throwable

--- Assets/Project/Scripts/Player.cs
+++ Assets/Project/Scripts/Player.cs
@@ -10,6 +10,9 @@ public class Player : MonoBehaviour
     public GameObject playerCamera;
 
     public float ballDistance = 2f;
+    public float ballThrowingForce = 5f;
+
+    private bool holdingBall = true;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,18 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ball.transform.position = playerCamera.transform.position + (playerCamera.transform.forward * ballDistance);
+        if (holdingBall)
+        { 
+            ball.transform.position = playerCamera.transform.position + (playerCamera.transform.forward * ballDistance);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                holdingBall = false;
+                ball.GetComponent<Rigidbody>().useGravity = true;
+                ball.GetComponent<Rigidbody>().AddForce(playerCamera.transform.forward * ballThrowingForce);
+            }
+        
+        }
+        
     }
 }
