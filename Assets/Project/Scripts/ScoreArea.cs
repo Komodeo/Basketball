6b69ca8ad714ee676909740057f2767b6b18ebc4
Komodeo <onlythebest@gmail.com>
2020-06-15 14:35:17 -0400

    lesson 8
    add scoring mechanism

---
+++ Assets/Project/Scripts/ScoreArea.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreArea : MonoBehaviour
+{
+
+    public GameObject effectObject;
+
+    private void OnTriggerEnter(Collider otherCollider)
+    {
+        if (otherCollider.GetComponent<Ball>() != null)
+        {
+            effectObject.SetActive(true);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        effectObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
